6fed2b25e763566c027de1b8db7b3d203ff6d5a7
Michael Zamani <michael.zamani1@gmail.com>
2014-12-09 10:54:05 -0500

    Minors modifications to RecipeTableViewCell

--- KitchenPal/RecipeTableViewCell.swift
+++ KitchenPal/RecipeTableViewCell.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class RecipeTableViewCell: UITableViewCell {
 
+    // The UILabel Storyboard object references
     @IBOutlet var recipeImageView: UIImageView!
     @IBOutlet var recipeName: UILabel!
-    //@IBOutlet var recipeCourses: UILabel!
     @IBOutlet var recipeRating: UILabel!
     
     override func awakeFromNib() {
