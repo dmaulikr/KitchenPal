2afa684a77e7b6a4e479e0da2be29d7c555f4549
Michael Zamani <michael.zamani1@gmail.com>
2014-12-10 11:02:43 -0500

    Fully implements the ViewMyRecipe view

--- KitchenPal/ViewMyRecipeViewController.swift
+++ KitchenPal/ViewMyRecipeViewController.swift
@@ -10,26 +10,116 @@ import UIKit
 
 class ViewMyRecipeViewController: UIViewController {
 
+    // Dictionary containing recipe data passed from the upstream view controller
+    var recipeData: NSDictionary?
+    
+    // Object references to the Storyboard elements
+    @IBOutlet var recipeImageView: UIImageView!
+    @IBOutlet var recipeNameLabel: UILabel!
+    @IBOutlet var totalTimeLabel: UILabel!
+    @IBOutlet var yieldLabel: UILabel!
+    @IBOutlet var ingredientsTextView: UITextView!
+    @IBOutlet var prepStepsTextView: UITextView!
+    @IBOutlet var spicyLabel: UILabel!
+    @IBOutlet var spicySlider: UISlider!
+    @IBOutlet var bitterLabel: UILabel!
+    @IBOutlet var bitterSlider: UISlider!
+    @IBOutlet var sweetLabel: UILabel!
+    @IBOutlet var sweetSlider: UISlider!
+    @IBOutlet var savoryLabel: UILabel!
+    @IBOutlet var savorySlider: UISlider!
+    @IBOutlet var saltyLabel: UILabel!
+    @IBOutlet var saltySlider: UISlider!
+    @IBOutlet var sourLabel: UILabel!
+    @IBOutlet var sourSlider: UISlider!
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Document directory path for the recipe image
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentDirectoryPath = paths[0] as String
 
-        // Do any additional setup after loading the view.
+        // Set the Recipe Name as the title and the label text
+        
+        var recipeName = recipeData!.objectForKey("name") as String
+        self.title = recipeName
+        recipeNameLabel.text = recipeName
+        
+        // Total time and yield labels
+        
+        var totalTime = recipeData!.objectForKey("totalTime") as String
+        totalTimeLabel.text = "Preparation Time: \(totalTime)"
+        var yield = recipeData!.objectForKey("yield") as String
+        yieldLabel.text = "Servings: \(yield)"
+        
+        // Set the recipe image
+        var recipeImagePath = documentDirectoryPath + "/" + (recipeData?.objectForKey("image") as String)
+        var recipeImage = UIImage(contentsOfFile: recipeImagePath)
+        recipeImageView.image = recipeImage
+        
+        // Set the ingredients list
+        var ingredientsArray = recipeData!.objectForKey("ingredientLines") as [String]
+        ingredientsTextView.text = "\n".join(ingredientsArray)
+        
+        // Set the preparation steps
+        var prepSteps = recipeData!.objectForKey("preparationSteps") as String
+        prepStepsTextView.text = prepSteps
+        
+        // Set the flavor slider values
+        var flavorDict = recipeData!.objectForKey("flavors") as NSDictionary
+        
+        let spicyValue = flavorDict.objectForKey("Spicy") as Float
+        spicySlider.value = spicyValue
+        let spicyText = String(format: "%.2f", spicyValue)
+        spicyLabel.text = "Spicy: \(spicyText)"
+        
+        let bitterValue = flavorDict.objectForKey("Bitter") as Float
+        bitterSlider.value = bitterValue
+        let bitterText = String(format: "%.2f", bitterValue)
+        bitterLabel.text = "Bitter: \(bitterText)"
+        
+        let sweetValue = flavorDict.objectForKey("Sweet") as Float
+        sweetSlider.value = sweetValue
+        let sweetText = String(format: "%.2f", sweetValue)
+        sweetLabel.text = "Sweet: \(sweetText)"
+        
+        let savoryValue = flavorDict.objectForKey("Savory") as Float
+        savorySlider.value = savoryValue
+        let savoryText = String(format: "%.2f", savoryValue)
+        savoryLabel.text = "Savory: \(savoryText)"
+        
+        let saltyValue = flavorDict.objectForKey("Salty") as Float
+        saltySlider.value = saltyValue
+        let saltyText = String(format: "%.2f", saltyValue)
+        saltyLabel.text = "Salty: \(saltyText)"
+        
+        let sourValue = flavorDict.objectForKey("Sour") as Float
+        sourSlider.value = sourValue
+        let sourText = String(format: "%.2f", sourValue)
+        sourLabel.text = "Sour: \(sourText)"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - View Nutition Info Pressed
+    
+    @IBAction func viewNutritionInfoPressed(sender: UIButton) {
+        
+        performSegueWithIdentifier("ShowNutrition", sender: self)
     }
     
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     
+        if segue.identifier == "ShowNutrition" {
+            
+            var controller: MyRecipeNutritionViewController = segue.destinationViewController as MyRecipeNutritionViewController
+            
+            controller.nutritionDict = recipeData!.objectForKey("nutrition") as? NSDictionary
+        }
     }
-    */
 
 }
