5338435f5da97535dde083482ce1dd448a8bf3e4
Michael Zamani <michael.zamani1@gmail.com>
2014-12-10 10:17:02 -0500

    Implements the MyRecipeNutrition view

--- KitchenPal/MyRecipeNutritionViewController.swift
+++ KitchenPal/MyRecipeNutritionViewController.swift
@@ -7,29 +7,79 @@
 //
 
 import UIKit
+import HealthKit
 
 class MyRecipeNutritionViewController: UIViewController {
-
+    
+    // Object reference to this project's AppDelegate
+    var appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+    
+    // Object reference to Storyboard elements
+    @IBOutlet var nutritionTextView: UITextView!
+    
+    // Nutrition data passed from the upstream view controller
+    var nutritionDict: NSDictionary?
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Nutrition Information"
+        
+        var allNutritionDataAsText: String = ""
+        
+        for quantityType in nutritionDict!.allKeys {
+            
+            // All quantityType keys begin with "HKQuantityTypeIdentifierDietary" 
+            // Create a substring beginning from index 30
+            
+            let index: String.Index = advance((quantityType as String).startIndex, 30)
+            
+            var descriptionText = (quantityType as String).substringFromIndex(index)
+            
+            descriptionText += ":"
+            
+            var valueAndUnit = nutritionDict!.objectForKey(quantityType) as NSArray
+            
+            var units: String = valueAndUnit.objectAtIndex(1) as String
+            
+            var value = String(format: "%.2f", (valueAndUnit.objectAtIndex(0) as Double))
+            
+            allNutritionDataAsText += " ".join([descriptionText, value, units])
+            
+            allNutritionDataAsText += "\n"
+        }
+        
+        nutritionTextView.text = allNutritionDataAsText
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Log In Health App Button Pressed
+    
+    @IBAction func logInHealthAppPressed(sender: UIButton) {
+        
+        for quantityType in nutritionDict!.allKeys {
+            
+            var nutritionType = HKQuantityType.quantityTypeForIdentifier(quantityType as String)
+            
+            var valueAndUnit = nutritionDict!.objectForKey(quantityType) as NSArray
+            
+            var value = valueAndUnit.objectAtIndex(0) as Double
+            var unitAbbrev: String = valueAndUnit.objectAtIndex(1) as String
+            
+            var unit = HKUnit(fromString: unitAbbrev)
+            
+            var quantity: HKQuantity = HKQuantity(unit: unit, doubleValue: value)
+            
+            var nutritionObject = HKQuantitySample(type: nutritionType!, quantity: quantity, startDate: NSDate(), endDate: NSDate())
+            
+            println(quantity)
+            println(nutritionType!)
+            
+            appDelegate.healthStore.saveObject(nutritionObject, withCompletion: nil)
+        }
+        
+        self.navigationController?.popViewControllerAnimated(true)
     }
-    */
-
 }
